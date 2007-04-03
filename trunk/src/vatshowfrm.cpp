cc447337e8f06d8e4794be2f6618fed6adc28e98
oprocopio <oprocopio@6c6712ea-782a-0410-9b53-9464c500c9f9>
2007-04-03 09:08:51 +0000

    nomsg

---
+++ trunk/src/vatshowfrm.cpp
@@ -0,0 +1,9 @@
+#include "vatshowfrm.h"
+//
+vatshowfrm::vatshowfrm( QWidget * parent, Qt::WFlags f) 
+	: QDialog(parent, f)
+{
+	setupUi(this);
+}
+//
+
---
+++ trunk/src/vatshowfrm.h
@@ -0,0 +1,18 @@
+#ifndef VATSHOWFRM_H
+#define VATSHOWFRM_H
+//
+#include "ui_vatshowfrm.h"
+//
+class vatshowfrm : public QDialog, public Ui::vatshowfrm
+{
+Q_OBJECT
+public:
+	vatshowfrm( QWidget * parent = 0, Qt::WFlags f = 0 );
+private slots:
+};
+#endif
+
+
+
+
+
