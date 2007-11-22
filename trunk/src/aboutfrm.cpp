8d1387cbebd82b49acf65ce8cc6427abd5e184a4
chmaster <chmaster@6c6712ea-782a-0410-9b53-9464c500c9f9>
2007-11-22 16:14:47 +0000

    * fixed problem with comilation error   * to long text in aboutfrm

--- trunk/src/aboutfrm.cpp
+++ trunk/src/aboutfrm.cpp
@@ -1,9 +1,28 @@
+#include <QtCore>
 #include "aboutfrm.h"
 //
 aboutfrm::aboutfrm( QWidget * parent, Qt::WFlags f) 
 	: QDialog(parent, f)
 {
 	setupUi(this);
+	QString info = QString( "" );
+
+	info += "Copyright (C) 2007 LINUX-BOX.ch";
+	info += "\n";
+	info += "Application: Orlando Procopio\n";
+	info += "Plugins: Alexander Saal\n";
+	label_2->setText( info );
+
+
+    QFile lic( QString::fromUtf8( ":/COPYING" ) );
+    if ( !lic.open( QIODevice::ReadOnly | QIODevice::Text ) ) {
+        return ;
+    }
+    QTextStream LicIn( &lic );
+    QString lineLic = LicIn.readAll();
+	lic.close();
+
+	textEditLicense->setPlainText( lineLic );
 }
 //
 
