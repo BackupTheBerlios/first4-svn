2e00ba374e88eb57b737897dd99f63069ff549d1
chmaster <chmaster@6c6712ea-782a-0410-9b53-9464c500c9f9>
2008-01-31 17:01:48 +0000

    * update aboutfrm.ui (compilation error

--- trunk/src/aboutfrm.cpp
+++ trunk/src/aboutfrm.cpp
@@ -8,14 +8,7 @@ aboutfrm::aboutfrm( QWidget * parent, Qt::WFlags f)
 	setupUi(this);
 	QString info = QString( "" );
 
-	info += "Copyright (C) 2007 LINUX-BOX.ch";
-	info += "\n\n";
-	info += "Application: Orlando Procopio\n";
-	info += "Plugins: Alexander Saal\n";
-	label_2->setText( info );
-
-
-    QFile lic( QString::fromUtf8( ":/COPYING" ) );
+    QFile lic( QString::fromUtf8( ":/license/COPYING" ) );
     if ( !lic.open( QIODevice::ReadOnly | QIODevice::Text ) ) {
         return ;
     }
